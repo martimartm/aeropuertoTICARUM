297ba9514505f17d8db9f8487003442c090a6e56
martimartm <marta.martinezmolina@hotmail.com>
2023-10-04 20:18:34 +0200

    Add sql query count_aviones

--- src/main/java/com/ticarum/aeropuerto/model/Aerolinea.java
+++ src/main/java/com/ticarum/aeropuerto/model/Aerolinea.java
@@ -1,5 +1,6 @@
 package com.ticarum.aeropuerto.model;
 import jakarta.persistence.*;
+import org.hibernate.annotations.Formula;
 
 @Entity
 @Table(name="Aerolinea")
@@ -12,11 +13,12 @@ public class Aerolinea {
 
     @Column(name="name_aerolinea", nullable = false, unique = true)
     private String name_aerolinea;
-//Consulta SQL
-//Número de aviones a la tabla avion que corresponda al id de la aerolinea
-    public Aerolinea(){
 
-    }
+    @Formula("SELECT COUNT(*) FROM Avion x WHERE x.id_aerolinea = id_aerolinea")
+    private Long count_aviones;
+
+
+    public Aerolinea(){}
     public Aerolinea (String name_aerolinea ){
         this.name_aerolinea=name_aerolinea;
     }
