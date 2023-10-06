53f5db93c602c2d52ff0b4b892feda1d1275b0da
martimartm <marta.martinezmolina@hotmail.com>
2023-10-06 17:36:45 +0200

    Add aerolinea repository

--- src/main/java/com/ticarum/aeropuerto/repository/AerolineaRepository.java
+++ src/main/java/com/ticarum/aeropuerto/repository/AerolineaRepository.java
@@ -1,10 +1,12 @@
 package com.ticarum.aeropuerto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.ticarum.aeropuerto.model.Aerolinea;
 
 @Repository
 public interface AerolineaRepository extends JpaRepository<Aerolinea,Long>{
-    
+    List<Aerolinea> findByName(String name_aerolinea);
 }
