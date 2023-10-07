74c346822bf31659038e7474e689094ec8a04dc3
martimartm <marta.martinezmolina@hotmail.com>
2023-10-07 21:40:49 +0200

    Add ResourceNotFoundException

---
+++ src/main/java/com/ticarum/aeropuerto/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ticarum.aeropuerto.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
