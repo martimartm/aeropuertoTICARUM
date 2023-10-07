c38e257db74ce070482d538557c785905e4b8434
martimartm <marta.martinezmolina@hotmail.com>
2023-10-07 20:13:03 +0200

    Add avion repository

--- src/main/java/com/ticarum/aeropuerto/repository/AvionRepository.java
+++ src/main/java/com/ticarum/aeropuerto/repository/AvionRepository.java
@@ -1,10 +1,15 @@
 package com.ticarum.aeropuerto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.ticarum.aeropuerto.model.Avion;
 
 @Repository
 public interface AvionRepository extends JpaRepository<Avion,Long>{
-    
+    @Query("SELECT a FROM Avion a WHERE a.aerolinea.idaerolinea = :aerolineaId")
+    public List<Avion> findByAerolineaId(@Param("aerolineaId") Long aerolineaId);
 }
