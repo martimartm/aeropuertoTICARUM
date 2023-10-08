4b0d78700ea205e92e8a33d9d4924dd59b047d13
martimartm <marta.martinezmolina@hotmail.com>
2023-10-08 21:41:51 +0200

    Add crear nuevo avion Devolver lista con aviones de una aerolinea

--- src/main/java/com/ticarum/aeropuerto/controllers/AvionController.java
+++ src/main/java/com/ticarum/aeropuerto/controllers/AvionController.java
@@ -1,16 +1,22 @@
 package com.ticarum.aeropuerto.controllers;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ticarum.aeropuerto.repository.AerolineaRepository;
 import com.ticarum.aeropuerto.repository.AvionRepository;
+import com.ticarum.aeropuerto.exception.ResourceNotFoundException;
+import com.ticarum.aeropuerto.model.Aerolinea;
 import com.ticarum.aeropuerto.model.Avion;
 
 
@@ -20,11 +26,51 @@ public class AvionController {
 
     @Autowired
     private AvionRepository avionRepository; 
+    @Autowired
+    private AerolineaRepository aerolineaRepository;
 
-      @GetMapping("/aviones/{aerolineaId}")
-   public ResponseEntity<List<Avion>> getAllAvionByaerolineaid(@PathVariable(value = "aerolineaId") Long aerolineaId) {
+    @Autowired
+    public AvionController(AerolineaRepository aerolineaRepository, AvionRepository avionRepository) {
+        this.aerolineaRepository = aerolineaRepository;
+        this.avionRepository = avionRepository;
+    }
 
-    List<Avion> avion = avionRepository.findByAerolineaId(aerolineaId);
-    return new ResponseEntity<>(avion, HttpStatus.OK);
-  }
-}
+    //Devuelve una lista de todos los aviones pertenecientes a una aerolinea
+    @GetMapping("/aviones/{aerolineaId}")
+    public ResponseEntity<?> getAllAvionByaerolineaid(@PathVariable(value = "aerolineaId") Long aerolineaId) {
+    
+        List<Avion> aviones = avionRepository.findByAerolineaId(aerolineaId);
+    
+        if (aviones.isEmpty()) {
+            String mensaje = "La aerolínea no tiene aviones";
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(mensaje);
+        } else {
+            return new ResponseEntity<>(aviones, HttpStatus.OK);
+        }
+    }
+
+       @PostMapping("/aviones/nuevo")
+       public Avion crearAvion(@PathVariable("name") String name, @RequestBody Avion nuevoAvion) {
+           //System.out.println(name);
+           Aerolinea aerolinea = aerolineaRepository.findByName(name);
+            //System.out.println(aerolinea);
+           if (aerolinea != null) {
+
+           nuevoAvion.setModelo_avion(nuevoAvion.getModelo_avion());
+           nuevoAvion.setCapacidad_avion(nuevoAvion.getCapacidad_avion());
+           nuevoAvion.setAerolinea(aerolinea);
+           nuevoAvion = avionRepository.save(nuevoAvion);
+            return nuevoAvion ;
+
+           } else {
+               throw new ResourceNotFoundException("Aerolinea no encontrada");
+           }
+       }
+   }
+   
+
+   
+   
+   
+
+
